da06d35b5fb6e0155f62d4e6a8c716de3d5545cd
Grzegorz Bułka <gbulka@tlen.pl>
2026-04-06 18:32:15 +0200

    Proximity sensor - detect distance alert.

--- arduino-sketches/sketch_20260406_proximity-sensor/sketch_20260406_proximity-sensor.ino
+++ arduino-sketches/sketch_20260406_proximity-sensor/sketch_20260406_proximity-sensor.ino
@@ -1,23 +1,41 @@
-#define trigPin 12
+#define PROXY_TTD 58 // proxy-sensor time-to-distance ratio
+#define triggerPin 12
 #define echoPin 11
+#define minDistance 10 // cm
+#define distanceTriggerTimeMiliseconds = 5000
+
+unsigned long distanceExceededMiliseconds = 0;
 
 void setup() {
   Serial.begin (9600);
-  pinMode(trigPin, OUTPUT); //Pin, do którego podłączymy trig jako wyjście
+  pinMode(triggerPin, OUTPUT); //Pin, do którego podłączymy trig jako wyjście
   pinMode(echoPin, INPUT); //a echo, jako wejście
 }
+
 void loop() {
-  long czas, dystans;
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  czas = pulseIn(echoPin, HIGH);
-  dystans = czas / 58;
-  
-  Serial.print(dystans);
-  Serial.println(" cm");
-  
+  unsigned long timestamp = millis();
   delay(500);
+
+  long distance = getDistanceCentimeters();  
+  if (distance >= minDistance) {
+    distanceExceededMiliseconds += timestamp - millis(); // TODO: This number will overflow (go back to zero), after approximately 50 days.
+    Serial.print(distance);
+    Serial.println(" cm [ALERT!]");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+}
+
+int getDistanceCentimeters(){
+  long time, distance;
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  time = pulseIn(echoPin, HIGH);
+  distance = time / PROXY_TTD;
+  return distance;
 }
